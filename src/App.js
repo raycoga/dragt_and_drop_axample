a80066ddb365a76111ea33ff87ebd572a56d60f6
raycoga <capram94@gmail.com>
2021-09-01 16:03:51 -0400

    simple drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import {
+  SortableContainer,
+  SortableElement,
+  arrayMove,
+} from "react-sortable-hoc";
+
+const getRandomValue = () => {
+  console.log(Math.floor(Math.random() * 400) + 1);
+  return Math.floor(Math.random() * 400) + 1;
+};
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((val, index) => ({
+  title: "Item " + index,
+  index: index,
+  id: `algo-${index}`,
+  imageSrc: `https://picsum.photos/180/180?random=${getRandomValue()}`,
+}));
 
 function App() {
+  const [Items, setItems] = useState(items);
+  useEffect(() => {
+    console.log(Items);
+  }, [Items]);
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    if (oldIndex !== newIndex) {
+      setItems(arrayMove(Items, oldIndex, newIndex));
+    }
+  };
+
+  const SortableItem = SortableElement(({ item }) => (
+    <div className="item">
+      <img src={item.imageSrc} />
+    </div>
+  ));
+
+  const SortableList = SortableContainer(({ items }) => (
+    <div className="container">
+      {items.map((item, index) => (
+        <SortableItem key={`${item.id}`} index={index} item={item} />
+      ))}
+    </div>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SortableList
+        items={Items}
+        onSortEnd={onSortEnd}
+        axis="xy"
+        helperClass="SortableHelper"
+      />
     </div>
   );
 }
